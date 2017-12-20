00721882530f1fa675dcfd640bc6ed90bb83f42d
osama miro <o.miro650@gmail.com>
2017-12-20 05:01:21 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
 # your code goes here
+def begins_with_r(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+      i += 1
+    end
+
+  begins_with_r(array) do |i|
+    if i.start_with?("r")
+    return   true
+    else
+      false
+    end
+  end
+end
